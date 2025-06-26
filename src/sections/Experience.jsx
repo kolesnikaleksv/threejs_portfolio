c1b10b5ae5cf3857ab76f841a210908bfe9d9ef1
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-06-26 17:29:36 +0300

    Add Develober component with the animation - idle

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,5 +1,9 @@
 import {Canvas} from "@react-three/fiber";
 import {workExperiences} from "../constants/index.js";
+import {OrbitControls} from "@react-three/drei";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import Developer from "../components/Developer.jsx";
 
 const Experience = () => {
   return (
@@ -9,7 +13,13 @@ const Experience = () => {
         <div className="work-container">
           <div className="work-canvas">
             <Canvas>
-
+              <ambientLight intensity={7}/>
+              <spotLight position={[10, 10, 10]} angle={0.15} penubra={1}/>
+              <directionalLight position={[10, 10, 10]} intensity={1}/>
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2}/>
+              <Suspense fallback={<CanvasLoader/>}>
+                <Developer position-y={-3} scale={3}/>
+              </Suspense>
             </Canvas>
           </div>
 
